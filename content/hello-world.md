659d25fefc59248653a3ef6e46221276ef892501
blha303 <stevensmith.ome@gmail.com>
2015-03-29 14:07:59 -0400

    Initial

---
+++ content/hello-world.md
@@ -0,0 +1,8 @@
+Title: Hello World!
+Authors: blha303
+Date: 2015-03-30 01:31
+Modified: 2015-03-30 02:02
+Tags: FirstPost
+Category: meta
+
+Hello World!
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'blha303'
+SITENAME = u'b3Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Perth'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Home', 'http://blha303.com.au'),
+         ('Keybase', 'http://keybase.io/blha303'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/blha303'),
+          ('GitHub', 'http://github.com/blha303'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
